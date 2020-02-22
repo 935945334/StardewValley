7458a2b302ed1cb106cdd6d0a66967bedd4ce7be
935945334 <935945334@qq.com>
2020-02-22 19:36:41 +0800

    123

--- Valley.js
+++ Valley.js
@@ -477,47 +477,47 @@ html2canvas(camera).then(function(canvas) {
     var strDataURI1 = oCavans.toDataURL();
     downLoadFn(strDataURI1);
 });
-//  //判断浏览器类型
-// function myBrowser() {
-// 	var userAgent = navigator.userAgent; //取得浏览器的userAgent字符串
-// 	var isOpera = userAgent.indexOf("Opera") > -1;
-// 	if(isOpera) {
-//     	return "Opera"
-// 	}; //判断是否Opera浏览器
-// 	if(userAgent.indexOf("Firefox") > -1) {
-//     	return "FF";
-// 	} //判断是否Firefox浏览器
-// 	if(userAgent.indexOf("Chrome") > -1) {
-//     	return "Chrome";
-// 	}
-// 	if(userAgent.indexOf("Safari") > -1) {
-//     	return "Safari";
-// 	} //判断是否Safari浏览器
-// 	if(userAgent.indexOf("compatible") > -1 && userAgent.indexOf("MSIE") > -1 && !isOpera) {
-//     	return "IE";
-// 	}; //判断是否IE浏览器
-// 	if(userAgent.indexOf("Trident") > -1) {
-//    	 return "Edge";
-// 	} //判断是否Edge浏览器
-// }
-// //IE浏览器图片保存本地
-// function SaveAs5(imgURL) {
-//     var oPop = window.open(imgURL, "", "width=1, height=1, top=5000, left=5000");
-//     for(; oPop.document.readyState != "complete";) {
-//         if(oPop.document.readyState == "complete") break;
-//     }
-//     oPop.document.execCommand("SaveAs");
-//     oPop.close();
-// }
-// function download(strDataURI1) {
-//     var link = document.getElementById("IsA");
-//     // link.innerHTML = 'download_canvas_image';
-//     link.download = 'mypainting.png';
-//     link.addEventListener('click', function(ev) {
-//     link.href = strDataURI1;
-//     }, false);
-//     // document.body.appendChild(link);
-// };
+ //判断浏览器类型
+function myBrowser() {
+	var userAgent = navigator.userAgent; //取得浏览器的userAgent字符串
+	var isOpera = userAgent.indexOf("Opera") > -1;
+	if(isOpera) {
+    	return "Opera"
+	}; //判断是否Opera浏览器
+	if(userAgent.indexOf("Firefox") > -1) {
+    	return "FF";
+	} //判断是否Firefox浏览器
+	if(userAgent.indexOf("Chrome") > -1) {
+    	return "Chrome";
+	}
+	if(userAgent.indexOf("Safari") > -1) {
+    	return "Safari";
+	} //判断是否Safari浏览器
+	if(userAgent.indexOf("compatible") > -1 && userAgent.indexOf("MSIE") > -1 && !isOpera) {
+    	return "IE";
+	}; //判断是否IE浏览器
+	if(userAgent.indexOf("Trident") > -1) {
+   	 return "Edge";
+	} //判断是否Edge浏览器
+}
+//IE浏览器图片保存本地
+function SaveAs5(imgURL) {
+    var oPop = window.open(imgURL, "", "width=1, height=1, top=5000, left=5000");
+    for(; oPop.document.readyState != "complete";) {
+        if(oPop.document.readyState == "complete") break;
+    }
+    oPop.document.execCommand("SaveAs");
+    oPop.close();
+}
+function download(strDataURI1) {
+    var link = document.getElementById("IsA");
+    link.innerHTML = 'download_canvas_image';
+    link.download = 'mypainting.png';
+    link.addEventListener('click', function(ev) {
+    link.href = strDataURI1;
+    }, false);
+    // document.body.appendChild(link);
+};
 function downLoadFn(url) {
     if(myBrowser() === "IE" || myBrowser() === "Edge") {
         SaveAs5(url);
